17de7aeba2f72191390e5d70bb337a6e68b3fcee
Vittor Faria Pereira <vittorfpereira@gmail.com>
2022-08-08 17:53:29 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 
+# gryphon-web-scrapping
+
 In the ./requirements.txt one should specify the dependencies between templates.
 
 in the ./template/requirements.txt one should specify only the external libraries.
--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open('metadata.json') as fr:
     metadata = json.load(fr)
 
 setuptools.setup(
-    name="",  # Name of the repository
+    name="gryphon-web-scraping",  # Name of the repository
     version="0.0.1",
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",  # Repository URL or externally maintained page
+    url="https://github.com/ow-gryphon/gryphon-web-scraping",  # Repository URL or externally maintained page
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=requirements,
