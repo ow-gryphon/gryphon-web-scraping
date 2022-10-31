917efd23a10483b4347fb263e8f34b2f3b5687a7
danielxwang90 <daniel.wang@oliverwyman.com>
2022-10-30 23:10:09 -0400

    update metadata

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('metadata.json') as fr:
 
 setuptools.setup(
     name="gryphon-web-scraping",  # Name of the repository
-    version="0.0.4",
+    version="0.0.5",
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
@@ -20,6 +20,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ow-gryphon/gryphon-web-scraping",  # Repository URL or externally maintained page
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=requirements,
 )
