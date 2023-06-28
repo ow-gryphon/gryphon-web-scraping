77e5cd754208454a5ac7af274c06e9e77cd63374
danielxwang90 <daniel.wang@oliverwyman.com>
2023-06-28 11:27:18 -0400

    moved utilities

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('metadata.json') as fr:
 
 setuptools.setup(
     name="gryphon-web-scraping",  # Name of the repository
-    version="0.1.0",
+    version="0.1.1",
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
